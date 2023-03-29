79955be7ffb3cf536df5a4bac1b77cf0ae240f35
knockinthecave <crane2356@naver.com>
2023-03-29 11:39:27 +0900

    Error Try 0329-1

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,5 +1,5 @@
 """
-Test custiom Django management commands.
+Test custom Django management commands.
 """
 from unittest.mock import patch
 
@@ -17,15 +17,18 @@ class CommandTest(SimpleTestCase):
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
+
         call_command('wait_for_db')
+
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_sleep, patched_check) :
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationError."""
-        patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True] 
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True] 
               
         call_command('wait_for_db')
+
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
-        
